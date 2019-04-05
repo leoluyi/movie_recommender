1f8bf68009022cdda0561cb41c174764fcda38c4
Kristian Rother <kristian@spiced-academy.com>
2019-04-05 10:43:22 +0200

    create cookiecutter project

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Movie Recommender in a project::
+
+    import movie_recommender
---
+++ movie_recommender/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Movie Recommender."""
+
+__author__ = """SPICED"""
+__email__ = 'kristian@spiced-academy.com'
+__version__ = '0.1.0'
